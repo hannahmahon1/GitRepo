ade3222d72254e2ba931be949fee9cdd8064a733
Hannah Mahon <mahonha@tcd.ie>
2019-11-28 18:01:42 +0000

    added a reference

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import './style.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+/*
+This code is adapted from the following tutorial https://medium.com/hackernoon/building-github-profile-analytics-using-react-part-1-37e03b0c3366
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+*/
 serviceWorker.unregister();
